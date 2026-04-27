0db58f265c8388ca451fdfb9bdb1c80f55e72eda
Матвей Бахматов <aniproject59@gmail.com>
2026-04-27 09:00:30 +0500

    feat(persistence): register persistence services

--- src/Iris.Persistence/DependencyInjection.cs
+++ src/Iris.Persistence/DependencyInjection.cs
@@ -1,7 +1,33 @@
-﻿namespace Iris.Persistence
-{
-    public class DependencyInjection
-    {
+using Iris.Application.Abstractions.Persistence;
+using Iris.Persistence.Database;
+using Iris.Persistence.Repositories;
+using Iris.Persistence.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace Iris.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddIrisPersistence(
+        this IServiceCollection services,
+        Action<DatabaseOptions> configureOptions)
+    {
+        var options = new DatabaseOptions();
+        configureOptions(options);
+        options.Validate();
+
+        services.AddSingleton(options);
+
+        services.AddDbContext<IrisDbContext>(dbContextOptions =>
+        {
+            dbContextOptions.UseSqlite(options.ConnectionString);
+        });
+
+        services.AddScoped<IConversationRepository, ConversationRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+
+        return services;
     }
 }
