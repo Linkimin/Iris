e05a9886cbfcffe8187f6984a62aa30b4d5b6a48
Матвей Бахматов <aniproject59@gmail.com>
2026-04-29 23:22:03 +0500

    feat(desktop): add application facade

--- src/Iris.Desktop/Services/IrisApplicationFacade.cs
+++ src/Iris.Desktop/Services/IrisApplicationFacade.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Iris.Application.Chat.SendMessage;
+using Iris.Domain.Conversations;
+using Iris.Shared.Results;
 
-namespace Iris.Desktop.Services
+namespace Iris.Desktop.Services;
+
+public sealed class IrisApplicationFacade : IIrisApplicationFacade
 {
-    internal class IrisApplicationFacade
+    private readonly SendMessageHandler _sendMessageHandler;
+
+    public IrisApplicationFacade(SendMessageHandler sendMessageHandler)
     {
+        _sendMessageHandler = sendMessageHandler;
+    }
+
+    public Task<Result<SendMessageResult>> SendMessageAsync(
+        ConversationId? conversationId,
+        string message,
+        CancellationToken cancellationToken)
+    {
+        return _sendMessageHandler.HandleAsync(
+            new SendMessageCommand(conversationId, message),
+            cancellationToken);
     }
 }
